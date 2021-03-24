431d7669e18afb7609e5a5cd11e09e9c7fe184b9
NikSol <Kalyamba06@mail.ru>
2021-03-24 21:50:15 +0600

    Домашнее задание третьего урока

--- script/script.js
+++ script/script.js
@@ -1,59 +1,83 @@
 'use strict';
 
 class GoodsItem {
-    constructor(title, price, img) {
-        this.img = img;
-        this.title = title;
+    constructor(product_name, price) {
+        this.product_name = product_name;
         this.price = price;
     }
     render() {
-        return `<div class="goods-item">
-                <img width = "100px" height = "100px" src = "${this.img}" alt = "${this.title}">
-                <h3>​${this.title}​</h3>
-                <p>​${this.price}$​</p>
+        return `<div class="goods-item">                
+                <h3>​${this.product_name}​</h3>
+                <p>​${this.price} &#x20bd;​</p>
                 <button class="card-button" type="button">В корзину</button>
                 </div>`;
     }
 }
 
+function makeGETRequest(url, callback) {
+    let xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+const API_URL = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses`;
+
 class GoodsList {
     constructor() {
         this.goods = [];
     }
-    fetchGoods() {
-        this.goods = [
-            { title: 'Shirt', price: 150, img: 'img/Shirt.jpg' },
-            { title: 'Socks', price: 50, img: 'img/Socks.jpeg' },
-            { title: 'Jacket', price: 350, img: 'img/Jacket.jpg' },
-            { title: 'Shoes', price: 250, img: 'img/Shoes.jpg' },
-        ];
+    fetchGoods(cb) {
+        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+            this.goods = JSON.parse(goods);
+            cb();
+        });
     }
+
     render() {
         let listHtml = '';
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price, good.img);
+            const goodItem = new GoodsItem(good.product_name, good.price, good.img);
             listHtml += goodItem.render();
         });
         document.querySelector('.goods-list').innerHTML = listHtml;
     }
 
 }
+
 const list = new GoodsList();
-list.fetchGoods();
-list.render();
+list.fetchGoods(() => {
+    list.render();
+    console.log(list);
+});
 
 class Cart {
     constructor() {
     }
-    render() { } //создание верстки корзины
-    delete() { } // очистка всей корзины
-    countTotalCost() { } //подсчет общей суммы заказа
-    send() { } // отправка данных на сервер
-}
-
-class CartItem {
-    constructor() {
+    getCartItems() {
+        makeGETRequest(`${API_URL}/getBasket.json`, (goods) => {
+            this.goods = JSON.parse(goods);
+        });
     }
-    addCartItem() { } // добавление товара в корзину
-    deleteCartItem() { } // удаление товара из корзины
+
+    addCartItem() {
+        makeGETRequest(`${API_URL}/addToBasket.json`, (goods) => {
+            this.goods = JSON.parse(goods);
+        });
+    }
+
+    deleteCartItem() {
+        makeGETRequest(`${API_URL}/deleteFromBasket.json`, (goods) => {
+            this.goods = JSON.parse(goods);
+        });
+    }
 }
