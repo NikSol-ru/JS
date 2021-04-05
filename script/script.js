338aa73d676948c78506d7cdb6a84c4d4281e65b
NikSol <Kalyamba06@mail.ru>
2021-04-05 15:25:05 +0600

    Домашнее задание пятого урока

--- script/script.js
+++ script/script.js
@@ -1,83 +1,51 @@
 'use strict';
 
-class GoodsItem {
-    constructor(product_name, price) {
-        this.product_name = product_name;
-        this.price = price;
-    }
-    render() {
-        return `<div class="goods-item">                
-                <h3>​${this.product_name}​</h3>
-                <p>​${this.price} &#x20bd;​</p>
-                <button class="card-button" type="button">В корзину</button>
-                </div>`;
-    }
-}
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const app = new Vue({
+    el: '#app',
+    data: {
+        goods: [],
+        filteredGoods: [],
+        searchLine: '',
+        isVisibleCart: false
+    },
+    methods: {
+        makeGETRequest(url, callback) {
+            var xhr;
+            if (window.XMLHttpRequest) {
+                xhr = new XMLHttpRequest();
 
-function makeGETRequest(url, callback) {
-    let xhr;
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            callback(xhr.responseText);
+            } else if (window.ActiveXObject) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    callback(xhr.responseText);
+                }
+            };
+            xhr.open('GET', url, true);
+            xhr.send();
+        },
+
+        filterGoods(value) {
+            const regexp = new RegExp(value, 'i');
+            this.filteredGoods = this.goods.filter(good =>
+                regexp.test(good.product_name));
+        },
+
+        openCart() {
+            this.isVisibleCart = true;
+        },
+
+        closeCart() {
+            this.isVisibleCart = false;
         }
-    };
-    xhr.open('GET', url, true);
-    xhr.send();
-}
+    },
 
-const API_URL = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses`;
-
-class GoodsList {
-    constructor() {
-        this.goods = [];
-    }
-    fetchGoods(cb) {
-        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+    mounted() {
+        this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
             this.goods = JSON.parse(goods);
-            cb();
+            this.filteredGoods = JSON.parse(goods);
         });
     }
-
-    render() {
-        let listHtml = '';
-        this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.product_name, good.price, good.img);
-            listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
-    }
-
-}
-
-const list = new GoodsList();
-list.fetchGoods(() => {
-    list.render();
-    console.log(list);
 });
-
-class Cart {
-    constructor() {
-    }
-    getCartItems() {
-        makeGETRequest(`${API_URL}/getBasket.json`, (goods) => {
-            this.goods = JSON.parse(goods);
-        });
-    }
-
-    addCartItem() {
-        makeGETRequest(`${API_URL}/addToBasket.json`, (goods) => {
-            this.goods = JSON.parse(goods);
-        });
-    }
-
-    deleteCartItem() {
-        makeGETRequest(`${API_URL}/deleteFromBasket.json`, (goods) => {
-            this.goods = JSON.parse(goods);
-        });
-    }
-}
